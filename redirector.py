c712129bdd81c5f3c32ec06696dfd10f61d9614e
Chris1320 <chris1320is@protonmail.com>
2022-12-22 13:21:32 +0800

    Fixed redirector.py
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- redirector.py
+++ redirector.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Final
 
 from flask import Flask
 from flask import abort
@@ -8,8 +9,23 @@ from flask import redirect
 from config_handler.simple import Simple
 
 
+SCHEMAS: Final[dict[str, str]] = {
+    "http": "http://",
+    "https": "https://"
+}
+
+
 app = Flask(__name__)
 redirect_url = None  # Will be updated when /api/update is contacted.
+
+
+def buildURL(url: str, https: bool = True):
+    """
+    Add schema to URL.
+    """
+
+    schema = SCHEMAS["https"] if https else SCHEMAS["http"]
+    return f"{schema}{url}" if "://" not in url else schema
 
 
 @app.errorhandler(404)
@@ -18,7 +34,12 @@ def error404(e):
     Redirect the request to the actual server because it might be there.
     """
 
-    return redirect(redirect_url + request.full_path if redirect_url is not None else url_for("firstrun"), code=301)
+    return redirect(
+        buildURL(redirect_url + request.full_path)
+        if redirect_url is not None
+        else url_for("index"),
+        code=301
+    )
 
 
 @app.errorhandler(503)
@@ -33,15 +54,10 @@ def error503(e):
         <h1>Service Unavailable</h1>
         <p>The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later.</p>
 
-        <h6><a href="{url_for("firstrun")}?admin=true">For the system administrator</a></h6>
+        <h6><a href="{url_for("index")}?admin=true">For the system administrator</a></h6>
     </body>
 </html>
 """
-
-
-@app.route("/favicon.ico")
-def favicon():
-    return ""  # TODO?
 
 
 @app.route("/")
@@ -50,51 +66,55 @@ def index():
     Redirect the user to the ngrok server. (or any server, really)
     """
 
-    return redirect(redirect_url if redirect_url is not None else url_for("firstrun"), code=301)
+    if redirect_url is not None:  # Redirect if redirect_url is set.
+        return redirect(buildURL(redirect_url), code=301)
 
+    else:
+        # Show a 503 error page if redirect_url is not set.
+        if not request.args.get("admin", False, type=bool):  # FIXME: I don't think it converts the GET param value to bool.
+            abort(503)  # Just abort if not admin.
 
-@app.route("/firstrun", methods=["GET"])
-def firstrun():
-    if not request.args.get("admin", False, type=bool):  # FIXME: I don't think it converts the GET param value to bool.
-        abort(503)  # Just abort if not admin.
-
-    return """\
-<html>
+        return """\
+<!doctype html>
+<html lang=en>
     <head>
-        <style type="text/css">
-            code {background-color: gray; color: white;}
-        </style>
+        <title>503 Service Unavailable</title>
     </head>
     <body>
         <h1>Not Yet Updated</h1>
 
         <p>The redirect URL hasn't been updated yet.</p>
-        <p>Run <code>tunnel.py</code> on your server to start.</p>
+        <p>Run <i>tunnel.py</i> on your server to start.</p>
     </body>
 </html>
 """
 
 
-@app.route("/api/update")
-def update():
+@app.route("/api/update/<key>")
+def update(key: str):
     global api_key
     global redirect_url
 
     request_api_key = request.args.get("key", None, type=str)
-    new_redirect_url = request.args.get("redirect_url", None, type=str)
-    new_api_key = request.args.get("api_key", None, type=str)
+    new_value = request.args.get("value", None, type=str)
 
-    if request_api_key != api_key:
+    if request_api_key != api_key:  # Validate API key
         abort(401)
 
-    if new_redirect_url is not None:
-        redirect_url = new_redirect_url
+    if key == "redirect_url":
+        if new_value is None:
+            abort(400)
+
+        redirect_url = new_value
         print(f"[i] Updated redirect URL to `{redirect_url}`.")
         return "OK"
 
-    elif new_api_key is not None:
-        api_key = new_api_key
-        print(f"[i] Updated API key.")
+    elif key == "api_key":
+        if new_value is None:
+            abort(400)
+
+        api_key = new_value
+        print("[i] Updated API key.")
         return "OK"
 
     abort(400)
